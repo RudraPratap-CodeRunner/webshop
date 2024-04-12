216fafa6db662ef6cb9ce1ee5cb555badec16724
RudraPratap-CodeRunner <98970737+RudraPratap-CodeRunner@users.noreply.github.com>
2024-04-12 21:16:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Hero from "./components/Hero/Hero";
+import Slider from "./components/Slider/Slider";
+import Header from "./components/header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Hero/>
+      <Slider/>
+    </div>
+  );
+}
+
+export default App;
