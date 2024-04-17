07416a24c68f4ecb00dae72f99c00a1af71a46e2
RudraPratap-CodeRunner <98970737+RudraPratap-CodeRunner@users.noreply.github.com>
2024-04-17 20:39:34 +0530

    code for making website responsive added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,15 @@
-import React from 'react' 
+import React, { useState } from 'react' 
 import css from './Header.module.css';
 import Logo from '../../assets/logo.png'
 import {CgShoppingBag} from 'react-icons/cg';
+// import {GoThreeBars} from 'react-icons/go'
+import { TfiAlignJustify } from "react-icons/tfi";
 
 const Header = () => {
+    const [ShowMenu,setShowMenu]=useState(false);
+    const toggleMenu=()=>{
+        setShowMenu((ShowMenu)=>!ShowMenu)
+    }
   return (
     <div className={css.container}>
         <div className={css.logo}>
@@ -11,8 +17,12 @@ const Header = () => {
             <span>webshop</span>
         </div>
         <div className={css.right}>
-            <div className={css.menu}>
-                <ul className={css.menu}>
+            <div className={css.bars} onClick={toggleMenu}>
+                {/* <GoThreeBars/> */}
+                <TfiAlignJustify />
+            </div>
+            
+                <ul className={css.menu} style={{display:ShowMenu? 'inherit':'none'}} >
                     <li>Collections</li>
                     <li>Brands</li>
                     <li>New</li>
@@ -20,7 +30,7 @@ const Header = () => {
                     <li>ENG</li>
                 </ul>
 
-            </div>
+            
 
             <input type="text" className={css.search} placeholder='search' />
 
